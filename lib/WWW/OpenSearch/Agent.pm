4a866f4ec22c2ce79187acc9e89d58f2c49d6d6a
Brian Cassidy <bricas@cpan.org>
2010-05-25 09:41:40 -0300

    prep release

--- lib/WWW/OpenSearch/Agent.pm
+++ lib/WWW/OpenSearch/Agent.pm
@@ -38,7 +38,7 @@ An alias for request()
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2009 by Tatsuhiko Miyagawa and Brian Cassidy
+Copyright 2005-2010 by Tatsuhiko Miyagawa and Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
--- lib/WWW/OpenSearch/Image.pm
+++ lib/WWW/OpenSearch/Image.pm
@@ -45,7 +45,7 @@ WWW::OpenSearch::Image - Object to represent an image
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2009 by Tatsuhiko Miyagawa and Brian Cassidy
+Copyright 2005-2010 by Tatsuhiko Miyagawa and Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
