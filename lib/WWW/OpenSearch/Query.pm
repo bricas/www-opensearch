606dfc2de62afabc8a3bae1a6e6e520441e4b246
Brian Cassidy <bricas@cpan.org>
2013-01-18 16:02:26 -0400

    prep release

--- lib/WWW/OpenSearch/Query.pm
+++ lib/WWW/OpenSearch/Query.pm
@@ -41,7 +41,7 @@ WWW::OpenSearch::Query - Object to represent a sample query
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2010 by Tatsuhiko Miyagawa and Brian Cassidy
+Copyright 2005-2013 by Tatsuhiko Miyagawa and Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
--- lib/WWW/OpenSearch/Response.pm
+++ lib/WWW/OpenSearch/Response.pm
@@ -94,7 +94,7 @@ is equal to $type.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2010 by Tatsuhiko Miyagawa and Brian Cassidy
+Copyright 2005-2013 by Tatsuhiko Miyagawa and Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
